c7f97738aeec43eb055397b7a7b189b6a888e8b0
Francisco Vizentin <franciscovizentin@gmail.com>
2019-09-28 19:25:02 +0200

    Creates weird polygon

--- src/app.js
+++ src/app.js
@@ -16,13 +16,12 @@ function generateSquare(x, y, size) {
     coordinates.push(new Coordinate(x+size, y));
     coordinates.push(new Coordinate(x+size, y+size));
     coordinates.push(new Coordinate(x, y+size));
-    console.log(coordinates);
-      return coordinates;
+
+    return coordinates;
 }
 
 function generateLinearRing(coordinates) {
     let linearRingArray = [...coordinates];
-    console.log(linearRingArray);
     linearRingArray.push(linearRingArray[0]);
     let linearRing = factory.createLinearRing(linearRingArray);
     return linearRing;
@@ -30,24 +29,26 @@ function generateLinearRing(coordinates) {
 
 const sketch = p5 => {
 
-    let shape = generateLinearRing(generateSquare(10, 10, 400));
-    let mainGeometry = factory.createPolygon(shape);
+    let initialShape = generateSquare(10, 10, 400);
 
     p5.setup = () => {
         p5.createCanvas(800, 600);
         p5.background(0);
-        p5.stroke(p5.random()*255);
-        p5.fill(p5.random()*255);
-
-
+        p5.stroke(255);
+        p5.fill(255, 20);
     };
 
     p5.draw = () => {
 
+        let mainGeometry = factory.createPolygon(generateLinearRing(initialShape));
+
+        p5.background(0);
+
         let mouseGeometry = factory.createPoint(new Coordinate(p5.mouseX, p5.mouseY));
 
-        p5.beginShape(p5.POLYGON);
-        let coordinates = shape._points._coordinates;
+        p5.beginShape(p5.TRIANGLE_STRIP);
+        console.log(mainGeometry);
+        let coordinates = mainGeometry._shell._points._coordinates;
         for (let i = 0; i < coordinates.length; i++) {
             let c = coordinates[i];
             p5.vertex(c.x, c.y);
@@ -58,10 +59,14 @@ const sketch = p5 => {
         let distanceOperator = new DistanceOp(mouseGeometry, mainGeometry);
         let distance = distanceOperator.distance();
         let nearestPoints = distanceOperator.nearestPoints();
-        console.log(nearestPoints);
-        p5.line(nearestPoints[0].x, nearestPoints[0].y, nearestPoints[1].x, nearestPoints[1].y);
+        p5.ellipse(nearestPoints[1].x, nearestPoints[1].y, 10);
 
         // p5.ellipse(c.x, c.y, 150, 150);
+    }
+
+    p5.mousePressed = () => {
+        let newCoordinate = new Coordinate(p5.mouseX, p5.mouseY);
+        initialShape.push(newCoordinate);
     }
 
 }
